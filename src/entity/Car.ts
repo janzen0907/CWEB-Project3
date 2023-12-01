2df7e23326d091174943db459b009af4d8eeb700
janzen0907 <janzen0907@saskpolytech.ca>
2023-12-01 11:52:51 -0600

    Added Car entity and created trader entity --JohnJ

--- src/entity/Car.ts
+++ src/entity/Car.ts
@@ -1,5 +1,6 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
+import {Entity, PrimaryGeneratedColumn, Column, OneToOne, JoinColumn} from 'typeorm'
 import {IsOptional, Length, IsNotEmpty, Max, Min} from 'class-validator'
+import {Trader} from "./Trader";
 
 @Entity()
 export class Car {
@@ -21,15 +22,37 @@ export class Car {
 	@Column('int', {})
 	@Max(2024, {message: 'Year must be earlier than 2025'})
 	@Min(1908, {message: 'Year must be later than 1908'})
+	@IsNotEmpty({message: "Year is required"})
 		year: number
 
-	@Column('nvarchar', {length: 50})
-	@Length(1, 50, {message: 'Color must not exceed $constraint2 characters in length'})
-	@IsOptional()
-		color: string
+	@Column('int', {})
+	@IsNotEmpty({message: "Kilometers is required"})
+		km: number
+
+	@Column('int', {})
+	@IsNotEmpty({message: "Price is required"})
+		price: number
+
+	@Column('nvarchar', {length: 3})
+	@Length(3, 3, {message: 'Transmission must be in format: AWD, RWD or FWD only'})
+	@IsNotEmpty({message: "Transmission is required"})
+		transmission: string
 
 	@Column('nvarchar', {length: 50})
-	@Length(1,50, {message: 'Engine must be between $constraint1 and $constraint2 characters'})
+	@Length(1, 50, {message: 'Drivetrain must be between $Constraint1 and $Constrain2'})
+	@IsNotEmpty({message: "Drivetrain is required"})
+		drivetrain: string
+
+	@Column('int', {})
 	@IsOptional()
-		engine: string
+		numUpvotes: number
+
+	@Column('int', {})
+	@IsOptional()
+		numDownvotes: number
+
+	@OneToOne(type => Trader) @JoinColumn()
+		traderID: number
+
+
 }
