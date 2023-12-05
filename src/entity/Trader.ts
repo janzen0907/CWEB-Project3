4c79ba7f67c3dd1c2e930fea968b764920896037
cheezbrik <147870779+cheezbrik@users.noreply.github.com>
2023-12-04 21:19:33 -0600

    trader

---
+++ src/entity/Trader.ts
@@ -0,0 +1,29 @@
+import {Column, Entity, PrimaryGeneratedColumn} from "typeorm";
+import {IsEmail, IsNotEmpty, IsOptional, length, Length} from "class-validator";
+
+
+@Entity()
+export class Trader{
+    @PrimaryGeneratedColumn()
+    @IsOptional()
+        id: number
+
+    @Column('varchar')
+    @IsEmail({}, {message: "Email must be in proper format "})
+    @IsNotEmpty({message: "Email is required"})
+        email: string
+
+    @Column('varchar', {length: 300})
+    @Length(1, 300, {message: 'Name must be between $Constraint1 and $Constraint2 charecters'})
+    @IsNotEmpty({message: "Name is required"})
+        name: string
+
+    @Column('int')
+    @IsOptional()
+        rating: number
+
+    @Column('int')
+    @IsOptional()
+        ratingCount: number
+
+}
