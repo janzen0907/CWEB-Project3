3aa9c7111e730aae5de5f4d421a57c93f7bb58eb
John J <janzen0907@saskpolytech.ca>
2023-12-06 17:53:42 -0600

    Had to change path names of traders

--- src/controller/TraderController.ts
+++ src/controller/TraderController.ts
@@ -4,7 +4,7 @@ import {validate, ValidatorOptions} from 'class-validator'
 import {AppDataSource} from '../data-source'
 import {Body, Delete, Get, JsonController, Param, Post, Put, Req, Res} from 'routing-controllers'
 import {Like} from 'typeorm'
-import {Trader} from "../entity/Trader";
+import {Trader} from '../entity/Trader'
 
 
 @JsonController()
@@ -18,9 +18,9 @@ export default class TraderController {
 	}
 
 	//Get request for the Car entity
-	@Get('/trader/:id*?' )
+	@Get('/traders/:id*?')
 	async read(@Req() req: Request, @Res() res: Response) {
-		if (req.params.id) return this.TraderRepo.findOne({where: {id: req.params.id}} )
+		if (req.params.id) return this.TraderRepo.findOne({where: {id: req.params.id}})
 		else {
 			const searchString = req.query.params
 			const findOptions: any = {order: {}, where: []}
@@ -41,40 +41,37 @@ export default class TraderController {
 		}
 	}
 
-	@Delete('/trader/:id')
+	@Delete('/traders/:id')
 	async delete(@Req() req: Request, @Res() res: Response) {
 		//Check the headers for the authorization token
 		const token = req.headers.authorization
 		//If token is Bearer admin then they can delete from the db
-		if(token == 'Bearer admin')
-		{
-			const TraderToRemove = await this.TraderRepo.findOne(  {where: {id: req.params.id}})
+		if (token == 'Bearer admin') {
+			const TraderToRemove = await this.TraderRepo.findOne({where: {id: req.params.id}})
 			res.statusCode = 204
 			if (TraderToRemove) return this.TraderRepo.remove(TraderToRemove)
-			else{
+			else {
 
 			}
 		}
 		//They do not have access return a message
-		else
-		{
-			await  res.status(403).json({message: 'You do not have access'})
+		else {
+			await res.status(403).json({message: 'You do not have access'})
 		}
 
 	}
 
-	@Put('/trader/:confirmid')
+	@Put('/traders/:confirmid')
 	async update(@Body() reqBody: any, @Param('confirmid') confirmid: number, @Res() res: Response, @Req() req: Request) {
 		//Check the headers for the authorization token
-		const token = req.headers.authorization
+		//const token = req.headers.authorization
 		//If token is Bearer admin then they can Update then entry in the DB
-		if(token == 'Bearer admin')
-		{
+		//if(token == 'Bearer admin')
+		{//
 			const TraderToUpdate = await this.TraderRepo.preload(reqBody)
-			if (!TraderToUpdate || TraderToUpdate.id != confirmid){
+			if (!TraderToUpdate || TraderToUpdate.id != confirmid) {
 				// do nothing
-			}
-			else {
+			} else {
 				const violations = await validate(TraderToUpdate, this.validOptions)
 				if (violations.length) {
 					res.statusCode = 422 // Unprocessable Entity
@@ -84,26 +81,27 @@ export default class TraderController {
 				}
 			}
 			//They do not have access return a message
-		} else{
-			await  res.status(403).json({message: 'You do not have access'})
+			//} else{
+			//await  res.status(403).json({message: 'You do not have access'})
 		}
 
 	}
 
-	@Post('/trader')
-	async create(@Body() reqBody:any,@Res() res:Response, @Req() req: Request) {
+	@Post('/traders')
+	async create(@Body() reqBody: any, @Res() res: Response, @Req() req: Request) {
 		//Check the headers for the authorization token
+		console.log(reqBody)
 		//const token = req.headers.authorization
 		//If token is Bearer admin then they can Update then entry in the DB
 		//if (token == 'Bearer admin') {
-			const TraderToCreate = Object.assign(new Trader(), reqBody)
-			const violations = await validate(TraderToCreate, this.validOptions)
-			if (violations.length) {
-				res.statusCode = 422 // Unprocessable Entity
-				return violations
-			} else {
-				return this.TraderRepo.save(TraderToCreate)
-			}
+		const TraderToCreate = Object.assign(new Trader(), reqBody)
+		const violations = await validate(TraderToCreate, this.validOptions)
+		if (violations.length) {
+			res.statusCode = 422 // Unprocessable Entity
+			return violations
+		} else {
+			return this.TraderRepo.save(TraderToCreate)
+		}
 		//}
 		//They do not have access return a message
 		//else
