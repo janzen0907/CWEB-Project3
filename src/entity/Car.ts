d4f00982a2461ed6560e71e0db17b26ca35bcc85
cheezbrik <147870779+cheezbrik@users.noreply.github.com>
2023-12-06 22:20:21 -0600

    this sucks lol

--- src/entity/Car.ts
+++ src/entity/Car.ts
@@ -2,10 +2,7 @@ import {
 	Entity,
 	PrimaryGeneratedColumn,
 	Column,
-	OneToOne,
 	JoinColumn,
-	PrimaryColumn,
-	OneToMany,
 	ManyToOne
 } from 'typeorm'
 import {IsOptional, Length, IsNotEmpty, Max, Min} from 'class-validator'
@@ -15,67 +12,65 @@ import {Trader} from './Trader'
 export class Car {
 	@PrimaryGeneratedColumn('increment')
 	@IsOptional()
-	id: number
+		id: number
 
 
 	@Column('nvarchar', {length: 50})
 	@Length(1, 50, {message: 'Make must be from $constraint1 to $constraint2 characters'})
 	@IsNotEmpty({message: 'Make is required'})
-	make: string
+		make: string
 
 	@Column('nvarchar', {length: 50})
 	@Length(1, 50, {message: 'Model must be from $constraint1 to $constraint2 characters'})
 	@IsNotEmpty({message: 'Model is required'})
-	model: string
+		model: string
 
 	@Column('int', {})
 	@Max(2024, {message: 'Year must be earlier than 2025'})
 	@Min(1908, {message: 'Year must be later than 1908'})
 	@IsNotEmpty({message: 'Year is required'})
-	year: number
+		year: number
 
 	@Column('int', {})
 	@IsNotEmpty({message: 'Kilometers is required'})
-	km: number
+		km: number
 
 	@Column('int', {})
 	@IsNotEmpty({message: 'Price is required'})
-	price: number
+		price: number
 
 	@Column('nvarchar', {length: 3})
 	@Length(3, 3, {message: 'Drivetrain must be in format: AWD, RWD or FWD only'})
 	@IsNotEmpty({message: 'Drivetrain is required'})
-	drivetrain: string
+		drivetrain: string
 
 	@Column('nvarchar', {length: 50})
 	@Length(1, 50, {message: 'Transmission must be between $Constraint1 and $Constrain2'})
 	@IsNotEmpty({message: 'Transmission is required'})
-	transmission: string
+		transmission: string
 
 
 	 @Column('int', {default: 0})
 	 @IsOptional()
-	 numUpVotes: number
-	// //
+	 	numUpVotes: number
+
 	 @Column('int', {default: 0})
 	 @IsOptional()
-	 numDownVotes: number
+	 	numDownVotes: number
 	// //
 	@Column()
-	@IsOptional()
-	traderEmail: string
+		traderEmail: string
 
 	@Column()
-	@IsOptional()
-	traderName: string
+		traderName: string
 
-	 @ManyToOne(() => Trader, (traderE) => traderE.email)
-	 @JoinColumn({ name: 'traderEmail' })
-	 traderE: Trader
+	 @ManyToOne(() => Trader)
+	 @JoinColumn({ name: 'traderEmail', referencedColumnName: 'email' })
+	 	traderE: Trader
 
-	@ManyToOne(() => Trader, (traderN) => traderN.name)
-	@JoinColumn({ name: 'traderName' })
-	traderN: Trader
+	@ManyToOne(() => Trader)
+	@JoinColumn({ name: 'traderName', referencedColumnName: 'name' })
+		traderN: Trader
 
 
 }
--- src/entity/Trader.ts
+++ src/entity/Trader.ts
@@ -1,29 +1,29 @@
-import {Column, Entity, PrimaryGeneratedColumn, Unique} from "typeorm";
-import {IsEmail, IsNotEmpty, IsOptional, length, Length} from "class-validator";
+import {Column, Entity, PrimaryGeneratedColumn, Unique} from 'typeorm'
+import {IsEmail, IsNotEmpty, IsOptional, length, Length} from 'class-validator'
 
 
 @Entity()
 export class Trader{
     @PrimaryGeneratedColumn('increment')
     @IsOptional()
-        id: number
+    	id: number
 
     @Column('varchar', {unique: true})
-    @IsEmail({}, {message: "Email must be in proper format "})
-    @IsNotEmpty({message: "Email is required"})
-        email: string
+    @IsEmail({}, {message: 'Email must be in proper format '})
+    @IsNotEmpty({message: 'Email is required'})
+    	email: string
 
     @Column('varchar', {length: 300, unique:true})
     @Length(1, 300, {message: 'Name must be between $Constraint1 and $Constraint2 charecters'})
-    @IsNotEmpty({message: "Name is required"})
-        name: string
+    @IsNotEmpty({message: 'Name is required'})
+    	name: string
 
     @Column('int')
     @IsOptional()
-        rating: number
+    	rating: number
 
     @Column('int')
     @IsOptional()
-        ratingCount: number
+    	ratingCount: number
 
 }
